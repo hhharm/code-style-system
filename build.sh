2c4be8c206237baa09a541b888a25bcf7b9c5481
Nataliya Tolokontseva <natal_tolok@mail.ru>
2020-01-19 10:22:52 +0300

    Perfect

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-#builds all scss files
+#prepare build files
 #run from app root
-sass --watch ./scss:./static
 
-#todo: use some minification tool
-#todo: linter, prettier...
-# cp -f static/* ../shri-2020-task-1/stub/build/
+#for me to copy
+#sass --watch ./scss:./static
+#cp  cp -f static/* ../shri-2020-task-1/stub/build/
+#cp -f build/* ../shri-2020-task-1/stub/build/
 
+sass ./scss:./static || echo "Couldn't compline scss";
+cp ./static/script.js ./build/
+npx postcss ./static/*.css --use autoprefixer -d ./autoprefix
+echo "check ./autoprefix/style.css that there is no error after autoprefixer"
+command="npx postcss ./autoprefix/style.css > ./build/style.css"
+echo -e "then run \n ${command}"
